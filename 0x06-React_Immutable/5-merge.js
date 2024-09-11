1ffbf33cf0af14914bec1a623382ecfb8a4df182
saraa93-bit <saraa93177228@gmail.com>
2024-09-12 01:34:23 +0300

    immutable

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,18 +1,15 @@
-import { List, Map } from 'immutable';
+import { Map, List } from 'immutable';
 
-// Concatenates two arrays into a List
 export function concatElements(page1, page2) {
-  return List(page1).concat(List(page2));
+  const list1 = List(page1);
+  const list2 = List(page2);
+
+  return list1.concat(list2);
 }
 
-// Merges two objects into a List, with page2 values overriding page1 values
 export function mergeElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
-  
-  // Merge map1 and map2, giving priority to map2 values if keys conflict
-  const mergedMap = map1.merge(map2);
-  
-  // Return the merged values as a List
-  return List(mergedMap.values());
+
+  return map1.merge(map2);
 }
